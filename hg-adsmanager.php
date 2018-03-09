049c37c3a6863cba7ad18e4a97fc7021a896e0aa
Jesú Márquez <jesumarquez@gmail.com>
2018-03-09 20:19:36 -0300

    class county in saparate file

--- hg-adsmanager.php
+++ hg-adsmanager.php
@@ -15,6 +15,8 @@
 
 defined( 'ABSPATH' ) or die( '¡Sin trampas!' );
 
+require_once('hg-adsmanager-country.php');
+
 /**
  * FILTER
  */
@@ -198,42 +200,5 @@ class HG_AdsManager_Widget extends WP_Widget {
         $instance = array();
         $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
         return $instance;
-    }
-}
-
-class HG_AdsManager_Country {
-    private $table_name;
-    private $wpdb;
-    public function __construct(){
-        global $wpdb;
-        $this->wpdb = $wpdb;
-        $this->table_name = $wpdb->prefix . 'hg_adsmanager_country';
-
-        $charset_collate = $wpdb->get_charset_collate();
-
-        $sql = "CREATE TABLE $this->table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-        name tinytext NOT NULL,
-        PRIMARY KEY  (id)
-        ) $charset_collate;";
-        
-        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-        dbDelta( $sql );
-    }
-
-    public function getAll(){
-        $result = $this->wpdb->get_results("SELECT name FROM {$this->table_name}", OBJECT );
-        return $result;
-    }
-
-    public function add($name){
-        $this->wpdb->insert(
-            $this->table_name, 
-            array(
-                'time' => current_time('mysql'),
-                'name' => $name
-            )
-        );        
     }
 }
