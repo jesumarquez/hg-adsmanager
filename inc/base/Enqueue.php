e4e36bd2d4ab2314564dab716ff977a8ce03b293
Jesú Márquez <jesumarquez@gmail.com>
2018-04-17 17:58:29 -0300

    rename methods on enqueue

--- inc/base/Enqueue.php
+++ inc/base/Enqueue.php
@@ -9,10 +9,10 @@ use \HG\Base\BaseController;
 class Enqueue extends BaseController {
     
     public function register() {
-        add_action( 'admin_enqueue_scripts', array( $this, 'enqueueCountryScripts' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
     }
 
-    public function enqueueCountryScripts() {
+    public function enqueue() {
         //enqueue all our scripts
         //wp_enqueue_style( 'mypluginstyle', $this->plugin_url . 'assets/mystyle.css' );
         wp_enqueue_script( 'hgcountryscript', $this->plugin_url . 'assets/country.js' );
