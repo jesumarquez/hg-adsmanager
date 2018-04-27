9fb593d12eb6ac514f50e30e24c61e4a1e2f5f21
Jesú Márquez <jesumarquez@gmail.com>
2018-04-27 11:50:42 -0300

    versioning database

--- inc/base/DataBase.php
+++ inc/base/DataBase.php
@@ -10,31 +10,42 @@ class DataBase {
         global $wpdb;
         $table_name = $wpdb->prefix . 'hg_adsmanager_country';
         $charset_collate = $wpdb->get_charset_collate();
-
+        
+        define('HG_ADSMANAGER_DB_VERSION', '0.1.0');
+        
         $sql[] = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-        name tinytext NOT NULL,
-        PRIMARY KEY  (id)
-        ) $charset_collate;";
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+            name tinytext NOT NULL,
+            PRIMARY KEY  (id)
+            ) $charset_collate;";
 
         $table_name = $wpdb->prefix . 'hg_adsmanager_customer';
         $sql[] = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-        name tinytext NOT NULL,
-        PRIMARY KEY  (id)
-        ) $charset_collate;";
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+            name tinytext NOT NULL,
+            PRIMARY KEY  (id)
+            ) $charset_collate;";
         
         require_once('../wp-admin/includes/upgrade.php' );
         dbDelta( $sql );
+        
+        add_option('HG_ADSMANAGER_DB_VERSION', HG_ADSMANAGER_DB_VERSION);
     }
 
     public static function uninstall() {
         global $wpdb;
-        $table_name = $wpdb->prefix . 'hg_adsmanager_country';
-        // $wpdb->query("DROP TABLE IF EXISTS $table_name");
-        $table_name = $wpdb->prefix . 'hg_adsmanager_customer';
-        // $wpdb->query("DROP TABLE IF EXISTS $table_name");
+        $tables_names = [
+            'country', 
+            'customer'
+        ];
+        
+        foreach ($name as $tables_names) {
+            $wpdb->query("DROP TABLE IF EXISTS $name");
+        }
+
+        if(get_option('HG_ADSMANAGER_DB_VERSION'))
+            delete_option( 'HG_ADSMANAGER_DB_VERSION' );
     }
 }
