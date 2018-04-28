d732bc052c26a14dee38f207713860fb8aafe6bc
Jesú Márquez <jesumarquez@gmail.com>
2018-04-28 17:54:23 -0300

    plugin_text_domain property

--- inc/base/BaseController.php
+++ inc/base/BaseController.php
@@ -8,10 +8,12 @@ class BaseController {
     public $plugin;
     public $plugin_path;
     public $plugin_url;
+    public $plugin_text_domain;
 
     public function __construct () {
         $this->plugin_path  = plugin_dir_path( dirname( __FILE__, 2 ) ); 
         $this->plugin_url   = plugin_dir_url ( dirname( __FILE__, 2 ) ); 
         $this->plugin       = plugin_basename( dirname( __FILE__, 3 ) . '/hg-adsmanager.php' ); 
+        $this->plugin_text_domain  = 'hg-adsmanager';
     }
 }
