e93fe35949bbe66f9f2b3205dd36ec659e516b59
Jesú Márquez <jesumarquez@gmail.com>
2018-05-03 17:47:36 -0300

    Upgrade DB, Foreign keys en tabla publications

--- inc/base/DataBase.php
+++ inc/base/DataBase.php
@@ -8,43 +8,71 @@ namespace HG\Base;
 class DataBase {
     public static function install() {
         global $wpdb;
-        $table_name = $wpdb->prefix . 'hg_adsmanager_country';
+        
+        $country_table_name     = $wpdb->prefix . 'hg_adsmanager_country';
+        $customer_table_name    = $wpdb->prefix . 'hg_adsmanager_customer';
+        $publication_table_name = $wpdb->prefix . 'hg_adsmanager_publication';
+
         $charset_collate = $wpdb->get_charset_collate();
         
-        define('HG_ADSMANAGER_DB_VERSION', 2);
+        define('HG_ADSMANAGER_DB_VERSION', 3);
+        $installed_db_version = get_option('HG_ADSMANAGER_DB_VERSION');
+
+        if(! $installed_db_version )
+        {
+            // COUNTRY
+            $sql[] = "CREATE TABLE $country_table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+                name tinytext NOT NULL,
+                PRIMARY KEY  (id)
+                ) $charset_collate;";
+
+            // CUSTOMER
+            $sql[] = "CREATE TABLE $customer_table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+                name tinytext NOT NULL,
+                PRIMARY KEY  (id)
+                ) $charset_collate;";
+
+            // PUBLICATION
+            $sql[] = "CREATE TABLE $publication_table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+                name tinytext NOT NULL,
+                customer_id mediumint(9) NOT NULL,
+                country_id mediumint(9) NOT NULL,
+                active bool NOT NULL,
+                image_url VARCHAR(2083) NOT NULL,
+                call_to_action_url VARCHAR(2083) NOT NULL,
+                start_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+                finish_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+                PRIMARY KEY  (id),
+                FOREIGN KEY (customer_id) REFERENCES $customer_table_name(id),
+                FOREIGN KEY (country_id) REFERENCES $country_table_name(id),
+                ) $charset_collate;";
+
+            require_once('../wp-admin/includes/upgrade.php' );
+            dbDelta( $sql );
+        }
+        else {
+            switch ($installed_db_version) {
+                case '2':
+                    $sql = "ALTER TABLE `$publication_table_name`
+                    ADD CONSTRAINT `FK_hg_adsmanager_publication_customer` FOREIGN KEY (`customer_id`) REFERENCES `$customer_table_name` (`id`)";
+                    $wpdb->query($sql);
         
-        $sql[] = "CREATE TABLE $table_name (
-            id mediumint(9) NOT NULL AUTO_INCREMENT,
-            time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-            name tinytext NOT NULL,
-            PRIMARY KEY  (id)
-            ) $charset_collate;";
+                    $sql = "ALTER TABLE `$publication_table_name`
+                    ADD CONSTRAINT `FK_hg_adsmanager_publication_country` FOREIGN KEY (`country_id`) REFERENCES `$country_table_name` (`id`)";
+                    $wpdb->query($sql);
 
-        $table_name = $wpdb->prefix . 'hg_adsmanager_customer';
-        $sql[] = "CREATE TABLE $table_name (
-            id mediumint(9) NOT NULL AUTO_INCREMENT,
-            time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-            name tinytext NOT NULL,
-            PRIMARY KEY  (id)
-            ) $charset_collate;";
-
-        $table_name = $wpdb->prefix . 'hg_adsmanager_publication';
-        $sql[] = "CREATE TABLE $table_name (
-            id mediumint(9) NOT NULL AUTO_INCREMENT,
-            time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-            name tinytext NOT NULL,
-            customer_id mediumint(9) NOT NULL,
-            country_id mediumint(9) NOT NULL,
-            active bool NOT NULL,
-            image_url VARCHAR(2083) NOT NULL,
-            call_to_action_url VARCHAR(2083) NOT NULL,
-            start_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-            finish_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-            PRIMARY KEY  (id)
-            ) $charset_collate;";
-
-        require_once('../wp-admin/includes/upgrade.php' );
-        dbDelta( $sql );
+                break;
+                default:
+                    # code...
+                    break;
+            }
+        }
         
         update_option('HG_ADSMANAGER_DB_VERSION', HG_ADSMANAGER_DB_VERSION);
     }
