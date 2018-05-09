204a103a61c80112673041ec3ed615828baa2d75
Jesú Márquez <jesumarquez@gmail.com>
2018-05-09 11:27:03 -0300

    get country code from client ip

--- inc/Widgets/AdsManager_Widget.php
+++ inc/Widgets/AdsManager_Widget.php
@@ -5,6 +5,7 @@ namespace HG\Widgets;
  * @package hg-adsmanager
  */
 
+use GeoIp2\Database\Reader;
 
 class AdsManager_Widget extends \WP_Widget {
     // class constructor
@@ -19,26 +20,48 @@ class AdsManager_Widget extends \WP_Widget {
 	// output the widget content on the front-end
 	public function widget( $args, $instance ) {
         echo $args['before_widget'];
+
+        //get country code
+        $ipaddress = '201.216.208.145'; // $this->get_client_ip();
+        $country_code = $this->get_country_code( $ipaddress );        
         
         $publicationEntity = new \HG\Base\Entities\PublicationEntity();
         $publication = $publicationEntity->getById(3);
-    //     $ipaddress = '';
-    // if (getenv('HTTP_CLIENT_IP'))
-    //     $ipaddress = getenv('HTTP_CLIENT_IP');
-    // else if(getenv('HTTP_X_FORWARDED_FOR'))
-    //     $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    // else if(getenv('HTTP_X_FORWARDED'))
-    //     $ipaddress = getenv('HTTP_X_FORWARDED');
-    // else if(getenv('HTTP_FORWARDED_FOR'))
-    //     $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    // else if(getenv('HTTP_FORWARDED'))
-    //     $ipaddress = getenv('HTTP_FORWARDED');
-    // else if(getenv('REMOTE_ADDR'))
-    //     $ipaddress = getenv('REMOTE_ADDR');
-    // else
-    //     $ipaddress = 'UNKNOWN';
+
         echo "<img src='{$publication->image_url}' />";
         
         echo $args['after_widget'];
+    }
+
+    function get_client_ip() {
+        $ipaddress = '';
+        
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+
+        return $ipaddress;
+    }
+
+    function get_country_code( $ip ) {
+        $result = null;
+
+        try {
+            $plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
+            $reader = new Reader( $plugin_path . 'inc/geoip2/GeoLite2-Country.mmdb' );
+            $result = $reader->country( $ip )->country->isoCode;
+        } catch (\Exception $e) {
+            // handle the exception
+        }
+        return $result;
     }
 }
